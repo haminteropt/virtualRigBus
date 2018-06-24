82ce865a44d66eb7618814d41069b7d92c216b99
DELENN\wa1go <darryl@hambus.org>
2018-06-24 16:55:06 -0500

    Looks like core doesn't have serial port yet.

---
+++ VirtualRigBus/KenwoodEmu/KenwoodEmu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace VirtualRigBus.KenwoodEmu
+{
+    public class KenwoodEmu
+    {
+        static SerialPort serialPort;
+        public void openPort(string portName, int speed)
+        {
+
+        }
+    }
+}
